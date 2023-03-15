932cecb15e5c05ab13c0876038660fcdede77350
Hanna Madsen <hannamymadsen89@gmail.com>
2023-03-15 11:48:25 +0100

    evil plan

---
+++ Pages/EvilPlan.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CI_CD.Pages;
+
+public class EvilPlan : PageModel
+{
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public EvilPlan(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
+
